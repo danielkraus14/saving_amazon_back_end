e1c4d07d58294d2734a5d00cf47e4d45f0975ca0
danielkraus14 <danielk14@servimatsrl.com.ar>
2022-08-23 13:14:42 -0300

    first commit

---
+++ src/api/adsmovil-user/controllers/adsmovil-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  adsmovil-user controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::adsmovil-user.adsmovil-user');
---
+++ src/api/adsmovil-user/routes/adsmovil-user.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * adsmovil-user router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::adsmovil-user.adsmovil-user');
